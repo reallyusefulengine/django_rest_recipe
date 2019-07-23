bd58e48dbdf1569d6ebc8bc8320aa06e31aa506a
reallyusefulengine <will.jh75@gmail.com>
2019-07-23 21:07:17 +1000

    finalised custom admin page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -8,14 +8,14 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'test@testsu.com',
-            password = 'test1234'
+            email='test@testsu.com',
+            password='test1234'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'test@orduser.com',
-            password = 'testpass123',
-            name = 'Mr Blobby'
+            email='test@orduser.com',
+            password='testpass123',
+            name='Mr Blobby'
         )
 
     def test_users_listed(self):
@@ -25,3 +25,15 @@ class AdminSiteTests(TestCase):
         print(f'Response is: {res}')
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """test the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
